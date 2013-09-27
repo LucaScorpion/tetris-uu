bf6aa2ea6f64df65e908b24b2934d4f0ba625fe6
lucascalzotto@gmail.com <lucascalzotto@gmail.com@f35a7dd4-081e-2e1b-0359-dbe00447de15>
2013-09-27 08:37:22 +0000

    Added InputState class

---
+++ trunk/Tetris/Tetris/Tetris/InputState.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace Tetris
+{
+    /// <summary>
+    /// Keeps the up-to-date input data
+    /// </summary>
+    public static class InputState
+    {
+        #region Fields
+        static KeyboardState currentKeyboard;
+        static KeyboardState previousKeyboard;
+        public static MouseState currentMouse;
+        static MouseState previousMouse;
+        #endregion
+
+        #region Methods
+        public static void update()
+        {
+            //Update Kayboard
+            previousKeyboard = currentKeyboard;
+            currentKeyboard = Keyboard.GetState();
+
+            //Update Mouse
+            previousMouse = currentMouse;
+            currentMouse = Mouse.GetState();
+        }
+        public static Boolean isKeyDown(Keys k)
+        {
+            if (currentKeyboard.IsKeyDown(k))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public static Boolean isKeyUp(Keys k)
+        {
+            if (currentKeyboard.IsKeyUp(k))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public static Boolean isKeyPressed(Keys k)
+        {
+            if (currentKeyboard.IsKeyDown(k) && previousKeyboard.IsKeyUp(k))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public static Boolean leftClick()
+        {
+            if (currentMouse.LeftButton == ButtonState.Pressed && previousMouse.LeftButton == ButtonState.Pressed)
+            {
+                return true;
+            }
+            return false;
+        }
+        public static Boolean rightClick()
+        {
+            if (currentMouse.RightButton == ButtonState.Pressed && previousMouse.RightButton == ButtonState.Pressed)
+            {
+                return true;
+            }
+            return false;
+        }
+        #endregion
+
+        #region Properties
+        #endregion
+    }
+}
