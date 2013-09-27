999e919bbd6b14bee4bf379bbc1d657f1a9cf939
tbiesaart@gmail.com <tbiesaart@gmail.com@f35a7dd4-081e-2e1b-0359-dbe00447de15>
2013-09-27 08:13:54 +0000

    Framework for world added

---
+++ trunk/Tetris/Tetris/Tetris/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Tetris
+{
+    public class GameManager
+    {
+        #region Fields
+        GameState currentGameState = GameState.StartScreen; //Default gamestate is startscreen
+        GameMode currentGameMode;
+
+
+        #endregion
+
+        #region Methods
+        #endregion
+
+        #region Constructors
+        #endregion
+
+        #region Properties
+        #endregion
+    }
+    public enum GameState
+    {
+        StartScreen, Playing, Menu, GameOver, Paused
+    }
+    public enum GameMode
+    {
+        Singleplayer, Multiplayer
+    }
+}
---
+++ trunk/Tetris/Tetris/Tetris/Shape.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Tetris
+{
+    public class Shape
+    {
+        #region Fields
+        #endregion
+
+        #region Methods
+        #endregion
+
+        #region Constructors
+        #endregion
+
+        #region Properties
+        #endregion
+    }
+}
