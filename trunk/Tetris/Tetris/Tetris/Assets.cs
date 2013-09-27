5baf07b44b9cf7129b6fcf1257b9bcfd9ec6cabe
tbiesaart@gmail.com <tbiesaart@gmail.com@f35a7dd4-081e-2e1b-0359-dbe00447de15>
2013-09-27 08:54:23 +0000

    Misc Changes

---
+++ trunk/Tetris/Tetris/Tetris/Assets.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+
+namespace Tetris
+{
+    /// <summary>
+    /// This static class contains several commonly used assets like sounds, textures and fonts.
+    /// </summary>
+    public static class Assets
+    {
+        #region Fields
+        public static Texture2D DummyTexture;
+        #endregion
+
+        #region Methods
+        public static void init(GraphicsDevice g)
+        {
+            DummyTexture = new Texture2D(g, 1, 1);
+            DummyTexture.SetData(new Color[] { Color.White });
+        }
+        #endregion
+    }
+}
--- trunk/Tetris/Tetris/Tetris/World.cs
+++ trunk/Tetris/Tetris/Tetris/World.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
 
 namespace Tetris
 {
     public class World
     {
         #region Fields
+        Dictionary<Point, Block> blocks = new Dictionary<Point, Block>();
+        Shape currentShape = new Shape();
         #endregion
 
         #region Methods
