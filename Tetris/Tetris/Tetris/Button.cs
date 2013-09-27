adf945456f6b78cc602ce688a469587dd1233f18
lucascalzotto <lucascalzotto@gmail.com>
2013-09-27 09:59:37 +0000

    Added buttons (not clickable yet)

--- Tetris/Tetris/Tetris/Button.cs
+++ Tetris/Tetris/Tetris/Button.cs
@@ -10,17 +10,16 @@ namespace Tetris
     class Button
     {
         #region Fields
-
         #endregion
 
         #region Methods
-        public void DrawButton(SpriteBatch s, int width, int height, Vector2 pos, String text, Color color)
-        {
-            s.Draw(new Texture2D(s.GraphicsDevice, 1, 1), new Rectangle((int)pos.X, (int)pos.Y, width, height), color);
-        }
         #endregion
 
         #region Constructors
+        public Button(SpriteBatch s, int width, int height, Vector2 pos, String text, Color color)
+        {
+            s.Draw(Assets.DummyTexture, new Rectangle((int)pos.X, (int)pos.Y, width, height), color);
+        }
         #endregion
 
         #region Properties
