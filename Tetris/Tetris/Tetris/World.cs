7ca641193209f79bd7e81c3a1133bed2a6196375
tbiesaart <tbiesaart@gmail.com>
2013-09-28 08:29:14 +0000

    Added movement again

--- Tetris/Tetris/Tetris/World.cs
+++ Tetris/Tetris/Tetris/World.cs
@@ -15,6 +15,7 @@ namespace Tetris
         Block[,] grid = new Block[rows, columns];
 
         Shape currentShape;
+        ControlMode controlMode;
 
         Rectangle rect = new Rectangle();
         int borderOffset = 10;
@@ -26,8 +27,11 @@ namespace Tetris
         {
             if (currentShape == null)
             {
-                currentShape = new Shape(this);
+                currentShape = new Shape(this, controlMode);
             }
+
+            //Update shape
+            currentShape.Update(this);
         }
         public void Draw(SpriteBatch spriteBatch)
         {
@@ -53,19 +57,24 @@ namespace Tetris
         }
         public Rectangle CalculateBlockRectangle(int x, int y)
         {
-            return new Rectangle(rect.X + borderOffset + x * rect.Width / columns, rect.Y + borderOffset + y * rect.Height / rows, (rect.Width - 2 * borderOffset) / columns, (rect.Height - 2 * borderOffset) / rows);
+            return new Rectangle(rect.X + borderOffset + x * (rect.Width - 2 * borderOffset) / columns, rect.Y + borderOffset + y * (rect.Height - 2 * borderOffset) / rows, (rect.Width - 2 * borderOffset) / columns, (rect.Height - 2 * borderOffset) / rows);
         }
         public Rectangle CalculateBlockRectangle(Point p)
         {
             return CalculateBlockRectangle(p.X, p.Y);
         }
+        public void AddBlock(Block block, Point location)
+        {
+            grid[location.Y, location.X] = block;
+        }
         #endregion
 
         #region Constructors
-        public World(Rectangle rect, int offset)
+        public World(Rectangle rect, int offset, ControlMode controlMode)
         {
             this.rect = rect;
             this.borderOffset = offset;
+            this.controlMode = controlMode;
         }
         #endregion
 
@@ -73,6 +82,7 @@ namespace Tetris
         public Shape CurrentShape { get { return currentShape; } set { currentShape = value; } }
         public int Rows { get { return rows; } set { if (value > 0) { rows = value; } } }
         public int Columns { get { return columns; } set { if (value > 0) { columns = value; } } }
+        public Block[,] Grid { get { return grid; } }
         #endregion
     }
 }
