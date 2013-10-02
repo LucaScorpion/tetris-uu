94a59ed48c7e1967a0ebb90b79cd6fdfb955037e
lucascalzotto@gmail.com <lucascalzotto@gmail.com@f35a7dd4-081e-2e1b-0359-dbe00447de15>
2013-10-02 12:02:13 +0000

    Added Menu class

---
+++ trunk/Tetris/Tetris/Tetris/Menu.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Tetris
+{
+    public class Menu
+    {
+        #region Fields
+        List<Button> buttonList;
+        #endregion
+
+        #region Methods
+        public void AddButton(Button button)
+        {
+            buttonList.Add(button);
+        }
+        public void Update()
+        {
+            foreach (Button b in buttonList)
+            {
+                b.Update();
+            }
+        }
+        public void Draw(SpriteBatch s)
+        {
+            foreach (Button b in buttonList)
+            {
+                b.Draw(s);
+            }
+        }
+        #endregion
+
+        #region Constructors
+        /// <summary>
+        /// Create a new menu with a list of buttons.
+        /// </summary>
+        public Menu(List<Button> buttonList)
+        {
+            this.buttonList = buttonList;
+        }
+        #endregion
+
+        #region Properties
+        #endregion
+    }
+}
