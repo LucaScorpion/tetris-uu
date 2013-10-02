0fef252ce79d6a9e55a7b7f7fe6b7992d84a1a31
lucascalzotto <lucascalzotto@gmail.com>
2013-10-02 11:34:24 +0000

    Finished button class, added start game button

--- Tetris/Tetris/Tetris/Button.cs
+++ Tetris/Tetris/Tetris/Button.cs
@@ -12,30 +12,40 @@ namespace Tetris
         #region Fields
         Rectangle rect;
         Color buttonColor;
+        Color hoverColor;
         String text;
         SpriteFont font;
         Color textColor;
         Texture2D texture;
+        Action method;
+        bool hover = false;
         #endregion
 
         #region Methods
         //Handle mouseclicks on the button
         public void Update()
         {
-            if (InputState.leftClick())
+            if (InputState.currentMouse.X > rect.Left && InputState.currentMouse.X < rect.Right && InputState.currentMouse.Y > rect.Top && InputState.currentMouse.Y < rect.Bottom)
             {
-                if (InputState.currentMouse.X > rect.Left && InputState.currentMouse.X < rect.Right && InputState.currentMouse.Y > rect.Top && InputState.currentMouse.Y < rect.Bottom)
+                //Hover
+                hover = true;
+                if (InputState.leftClick())
                 {
-
+                    method.Invoke();
                 }
             }
+            else
+                hover = false;
         }
         public void Draw(SpriteBatch s)
         {
             //Draw the button
             if (texture == null)
             {
-                s.Draw(Assets.Textures.DummyTexture, rect, buttonColor);
+                if (hover)
+                    s.Draw(Assets.Textures.DummyTexture, rect, hoverColor);
+                else
+                    s.Draw(Assets.Textures.DummyTexture, rect, buttonColor);
             }
             else
             {
@@ -47,21 +57,24 @@ namespace Tetris
         #endregion
 
         #region Constructors
-        public Button(Rectangle rect, Color buttonColor, String text, SpriteFont font, Color textColor)
+        public Button(Rectangle rect, Color buttonColor, Color hoverColor, String text, SpriteFont font, Color textColor, Action method)
         {
             this.rect = rect;
             this.buttonColor = buttonColor;
+            this.hoverColor = hoverColor;
             this.text = text;
             this.font = font;
             this.textColor = textColor;
+            this.method = method;
         }
-        public Button(Rectangle rect, Texture2D texture, String text, SpriteFont font, Color textColor)
+        public Button(Rectangle rect, Texture2D texture, String text, SpriteFont font, Color textColor, Action method)
         {
             this.rect = rect;
             this.texture = texture;
             this.text = text;
             this.font = font;
             this.textColor = textColor;
+            this.method = method;
         }
         #endregion
 
